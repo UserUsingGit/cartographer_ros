c382676357716b662b96fb6952d32a0f80c84ff2
UserUsingGit <52501922+UserUsingGit@users.noreply.github.com>
2019-07-29 13:50:21 +0200

    Add files via upload

--- cartographer_ros/configuration_files/kit_2d.lua
+++ cartographer_ros/configuration_files/kit_2d.lua
@@ -42,31 +42,51 @@ options = {
   landmarks_sampling_ratio = 1.,
 }
 
+-- GENERAL
 MAP_BUILDER.use_trajectory_builder_2d = true
 
+-- LOCAL SLAM
+TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.05  --0.15
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 1e-3
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 1e-3
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.occupied_space_weight = 1e5 --2e1 before, worked
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 100
 TRAJECTORY_BUILDER_2D.use_imu_data = false
+-- Nr ROS msgs that make one complete scan, Kitti (Velodyne): 1 pointcloud/msg 
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
+-- Define amount of scans per submap
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 20 
+-- Online SLAM
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = false
+-- TRAJECTORY_BUILDER_nD.real_time_correlative_scan_matcher.linear_search_window
+-- TRAJECTORY_BUILDER_nD.real_time_correlative_scan_matcher.angular_search_window
+-- TRAJECTORY_BUILDER_nD.real_time_correlative_scan_matcher.translation_delta_cost_weight
+-- TRAJECTORY_BUILDER_nD.real_time_correlative_scan_matcher.rotation_delta_cost_weight
+-- Tweaking Performance of 2D setup
+-- TRAJECTORY_BUILDER_3D.ceres_scan_matcher.ceres_solver_options.num_threads = 6
+TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.1  -- 0.05
+TRAJECTORY_BUILDER_2D.min_z = -1. -- -1.5
+TRAJECTORY_BUILDER_2D.max_z = 1.  -- 1.5
+
+-- -- GLOBAL SLAM
+POSE_GRAPH.optimize_every_n_nodes = 0 
+-- POSE_GRAPH.max_num_final_iterations = 200
+-- -- MAP_BUILDER.num_background_threads = 7
+-- POSE_GRAPH.optimization_problem.huber_scale = 5e2 --5e2
+-- POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
+-- POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 15
+-- POSE_GRAPH.constraint_builder.min_score = 0.7 -- 0.62
+-- POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66
+
+-- -- Added enabling loop closure on Teraki datasets
+-- POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 50.
+-- POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(45.)
+
+return options
+
+
 --TRAJECTORY_BUILDER_2D.imu_gravity_time_constant = 10.
 --TRAJECTORY_BUILDER_2D.min_range = 2.
 --TRAJECTORY_BUILDER_2D.max_range = 50.
--- Number of ROS messages that make one complete pointcloud (Kitti dataset: one pointcloud gets sent in one message) 
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
---TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.1
--- Online SLAM
-TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+-- Search Window for local SLAM
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.
--- Tweaking Performance of 2D setup
---TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.05
--- Define how many scans should be taken to create a submap
---TRAJECTORY_BUILDER_2D.submaps.num_range_data = 10
-TRAJECTORY_BUILDER_2D.min_z = -0.5
-TRAJECTORY_BUILDER_2D.max_z = 0.5
-
-MAP_BUILDER.num_background_threads = 7
-POSE_GRAPH.optimization_problem.huber_scale = 5e2
-POSE_GRAPH.optimize_every_n_nodes = 35
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
-POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
-POSE_GRAPH.constraint_builder.min_score = 0.62
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66
-
-return options
